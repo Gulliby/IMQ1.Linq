9f13142dfa97ce270ef581890aefbbb60be7c730
Ilya_Myalik <Ilya_Myalik@epam.com>
2017-05-09 16:43:16 +0400

    Add some tasks

--- Task/Task/LinqSamples.cs
+++ Task/Task/LinqSamples.cs
@@ -27,39 +27,59 @@ namespace SampleQueries
 
 		[Category("Restriction Operators")]
 		[Title("Where - Task 1")]
-		[Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
+		[Description("Выдайте список всех клиентов, чей суммарный оборот (сумма всех заказов) превосходит некоторую величину X. " +
+		             "Продемонстрируйте выполнение запроса с различными X (подумайте, можно ли обойтись без копирования запроса несколько раз)")]
 		public void Linq1()
 		{
 			int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+		    var xlist = new List<int> { 5, 7 };
 
-			var lowNums =
-				from num in numbers
-				where num < 5
-				select num;
-
-			Console.WriteLine("Numbers < 5:");
-			foreach (var x in lowNums)
-			{
-				Console.WriteLine(x);
-			}
+		    xlist.ForEach(x =>
+		        {
+                    Console.WriteLine($@"Number < {x}");
+		            numbers
+		                .Where(element => element > x)
+		                .ToList()
+		                .ForEach(element => Console.WriteLine("{0}", element));
+		        }
+		    );
 		}
 
 		[Category("Restriction Operators")]
 		[Title("Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
+		[Description("Для каждого клиента составьте список поставщиков, находящихся в той же стране и том же городе")]
 
 		public void Linq2()
 		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
+            dataSource.Customers.ForEach(customer =>
+            {
+                Console.WriteLine($@"Customer city: {customer.City};  Customer country: {customer.Country}");
+                dataSource.Suppliers
+                .Where(supplier => supplier.City.Equals(customer.City) && supplier.Country.Equals(customer.Country)).ToList()
+                .ForEach(ObjectDumper.Write);
+            });
+        }
 
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
+	    [Category("Restriction Operators")]
+	    [Title("Where - Task 3")]
+	    [Description("Найдите всех клиентов, у которых были заказы, превосходящие по сумме величину X")]
 
-	}
+	    public void Linq3()
+	    {
+	        decimal xMax = 1000;
+            dataSource.Customers
+                .Where(customer => customer.Orders.Sum(order => order.Total) > xMax).ToList()
+                .ForEach(ObjectDumper.Write);
+	    }
+
+	    [Category("Restriction Operators")]
+	    [Title("Where - Task 4")]
+	    [Description("Выдайте список клиентов с указанием, начиная с какого месяца какого года они стали клиентами (принять за таковые месяц и год самого первого заказа)")]
+
+	    public void Linq4()
+	    {
+	        decimal xMax = 1000;
+	        dataSource.Customers.
+	    }
+    }
 }
